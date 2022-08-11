f5ebf663cfd21ee3d7f92331170c8609c53d916b
Smerlyn Garcí <smerlyngarcia@presidencia.gob.do>
2022-08-11 09:03:14 -0400

    #158 - Creando pruebas unitarias

---
+++ WebAPIPeliculas/WebAPIPelicuals.Tests/PruebasUnitarias/GenerosControllerTests.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebAPIPeliculas.Tests.PruebasUnitarias
+{
+    [TestClass]
+    public class GenerosControllerTests : BasePruebas
+    {
+        
+    }    
+}
